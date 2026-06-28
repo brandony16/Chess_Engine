4428e29d77eecef23da6a248e4b55f20332aaef7
Brandon Young <108242812+brandony16@users.noreply.github.com>
2026-06-28 12:36:01 -0400

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', 
+  base: "/Chess_Engine/",
   plugins: [react(), tailwindcss()],
   test: {
-    testTimeout: 0
-  }
-})
+    testTimeout: 0,
+  },
+});
